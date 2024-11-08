f8b34a0e802bd82eb93a63195e6396397ce33957
EndritThaci <154618110+EndritThaci@users.noreply.github.com>
2024-11-08 15:17:59 +0100

    Funksioni per menaxhimin e klienteve me qasje te plote pjesa e pare

--- projekti/server.py
+++ projekti/server.py
@@ -30,3 +30,44 @@ def broadcast_message(sender_socket, message):
                     client.send(message.encode('utf-8'))
                 except Exception as e:
                     print(f"[Error] Cannot send message to a client: {e}")
+
+# Funksioni per menaxhimin e klienteve me qasje te plote
+def handle_full_access(client_socket, client_address):
+    client_ip = client_address[0]
+    print(f"[Full Access] {client_ip} ka qasje te plote.")
+    log_request(client_ip, "Klienti ka qasje te plote.")
+    last_activity = time.time()
+
+    try:
+        while True:
+            client_socket.send(b"[Prioritet] Komanda: ")
+            client_socket.settimeout(INACTIVITY_TIMEOUT)
+
+            try:
+                command = client_socket.recv(1024).decode('utf-8')
+                last_activity = time.time()
+            except socket.timeout:
+                client_socket.send(b"Ju jeni joaktiv! A jeni ende aty? (12 sekonda per pergjigje): ")
+                client_socket.settimeout(RESPONSE_TIMEOUT)
+
+                try:
+                    response = client_socket.recv(1024).decode('utf-8')
+                    if response.lower() in ['po', 'yes', 'y']:
+                        client_socket.send(b"Faleminderit per pergjigjen!\n")
+                        last_activity = time.time()
+                        client_socket.settimeout(None)
+                        continue
+                    else:
+                        client_socket.send(b"Lidhja juaj do te mbyllet.\n")
+                        break
+                except socket.timeout:
+                    client_socket.send(b"Ju u larguat per shkak te mungeses se aktivitetit.\n")
+                    break
+            #if command == "shutdown":
+            #elif command.startswith("kick"):
+            #elif command.startswith("broadcast"):
+            #elif command.startswith("open"):
+    except Exception as e:
+        print(f"[Error] Problem me klientin me qasje te plote: {e}")
+    finally:
+        client_socket.close()
